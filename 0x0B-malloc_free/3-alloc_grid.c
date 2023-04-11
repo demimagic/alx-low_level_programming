511cc6b901e49d9f865b59dad23c14452a3e6267
Demilade Ajagbonna <dt5351@gmail.com>
2023-04-11 14:02:04 -0700

    betty 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,86 +1,43 @@
 #include "main.h"
-
 #include <stdlib.h>
 
 /**
-
- * alloc_grid - nested loop to make grid
-
- * @width: width input
-
- * @height: height input
-
- * Return: pointer to 2 dim. array
-
+ * alloc_grid - creates a 2D array of integers
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: pointer to the newly created 2D array
  */
-
 int **alloc_grid(int width, int height)
-
 {
-
-	int **mee;
-
+	int **grid;
 	int x, y;
 
-
-
 	if (width <= 0 || height <= 0)
-
 		return (NULL);
 
+	grid = malloc(sizeof(int *) * height);
 
-
-	mee = malloc(sizeof(int *) * height);
-
-
-
-	if (mee == NULL)
-
+	if (grid == NULL)
 		return (NULL);
-
-
 
 	for (x = 0; x < height; x++)
-
 	{
+		grid[x] = malloc(sizeof(int) * width);
 
-		mee[x] = malloc(sizeof(int) * width);
-
-
-
-		if (mee[x] == NULL)
-
+		if (grid[x] == NULL)
 		{
-
 			for (; x >= 0; x--)
+				free(grid[x]);
 
-				free(mee[x]);
-
-
-
-			free(mee);
-
+			free(grid);
 			return (NULL);
-
 		}
-
 	}
-
-
 
 	for (x = 0; x < height; x++)
-
-	{
-
 		for (y = 0; y < width; y++)
+			grid[x][y] = 0;
 
-			mee[x][y] = 0;
-
-	}
-
-
-
-	return (mee);
-
+	return (grid);
 }
-
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,42 +1,23 @@
-#include <stdio.h>
-
+#include "main.h"
 #include <stdlib.h>
 
-#include "main.h"
-
 /**
-
- * free_grid - frees 2d array
-
- * @grid: 2d grid
-
- * @height: height dimension of grid
-
- * Description: frees memory of grid
-
- * Return: nothing
-
+ * free_grid - frees a 2D grid
  *
-
+ * @grid: 2D grid to free
+ * @height: height of grid
+ *
+ * Description: Frees the memory allocated for a 2D grid of integers.
  */
 
 void free_grid(int **grid, int height)
-
 {
-
 	int i;
 
-
-
 	for (i = 0; i < height; i++)
-
 	{
-
 		free(grid[i]);
-
 	}
 
 	free(grid);
-
 }
-
